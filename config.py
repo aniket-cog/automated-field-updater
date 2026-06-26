ff6a83ddbbd85bcc7e9b1bdbbe1126cbedba3ca2
aniket-cog <aniket.saha@cognizant.com>
2026-06-26 17:33:17 +0530

    Add configuration settings for project milestones

---
+++ config.py
@@ -0,0 +1,39 @@
+MAIN_TABLE_INDEX = 0
+
+WORD_FIELDS = {
+    "Total Cost" : (13,1),
+    "Project Start Date": (29,1),
+    "Project End Date": (32,1),
+    "RELEASE Start Date": (68,1),
+    "RELEASE End Date": (70,1),
+    "SOW Approved by": (116,1),
+    "SOW Approved on": (119,1)
+}
+
+MILESTONE_PARENT_ROW = 20
+MILESTONE_PARENT_COLUMN = 1
+MILESTONE_TABLE_INDEX = 0
+MILESTONE_HEADER_ROW = 0
+
+MILESTONE_COLUMNS = {
+    "name": 0,
+    "date": 1,
+    "monthly": 2,
+    "quality": 3,
+    "invoice": 4
+}
+
+DUPLICATE_COLUMNS = [1,2]
+
+EXCEL_COMMON_FIELDS = {
+    "Project Start Date": "C10",
+    "Project End Date": "C11",
+    "Total Cost": "G7",
+}
+
+TEAM_NAME_COLUMN = "A"
+SPRINT_CYCLE_COLUMN = "I"
+ALLOCATION_COLUMN = "J"
+EXCEPTIONAL_RATE_COLUMN = "L"
+RESOURCE_START_ROW = 19
+
